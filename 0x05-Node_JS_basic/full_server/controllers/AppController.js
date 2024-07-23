780508e5e2971e61be1bc136996bcc6f76a58fe5
kamva-hanisi <lucashanisi@gmail.com>
2024-07-23 21:18:46 +0000

    structure of the server

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,9 +1,11 @@
-#!/usr/bin/node
-
+/**
+ * single file is not sustainable
+ */
 class AppController {
   static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
   }
 }
 
+export default AppController;
 module.exports = AppController;
